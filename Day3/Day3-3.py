8378f6d5fc27c6962df0bf2c2dd10a0732029576
pundudac <dpundure@gmail.com>
2022-02-28 17:37:20 +0200

    added day2-3

---
+++ Day3/Day3-3.py
@@ -0,0 +1,16 @@
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+c = int(input("Enter number: "))
+
+if a <= b <= c:
+    print(a, b, c)
+elif b <= c <= a:
+    print(b, c, a)
+elif b <= a <= c:
+    print(b, c, a)
+elif c <= a <= b:
+    print(c, a, b)
+elif c <= b <= a:
+    print(c, b, a)
+else:
+    print(a, c, b)
