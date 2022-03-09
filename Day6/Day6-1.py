c64d73f38a12d419f30c3a8bfa0c14ef8b3b76a2
pundudac <dpundure@gmail.com>
2022-03-09 16:58:41 +0200

    added day6-1

--- Day6/Day6-1.py
+++ Day6/Day6-1.py
@@ -7,7 +7,7 @@ while True:
     else:
         numbers_list.append(float(numbers))
         average_number = sum(numbers_list)/len(numbers_list)
-        print("Average number of all entered values", numbers_list, "is: ", average_number) # prints all numbers and average
+        print("Average number of all entered values", numbers_list, "is:", average_number) # prints all numbers and average
         print("TOP3 numbers you entered are:", sorted(numbers_list)[-3:],
               "and average number of all entered numbers is:", average_number) # prints top3 and average
         print("BOTTOM3 numbers you entered are:", sorted(numbers_list)[:3],
