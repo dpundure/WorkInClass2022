f1c486b86cbacb8dbeb06dcdb2d77c6e215c724f
pundudac <dpundure@gmail.com>
2022-04-25 19:58:08 +0300

    final project

---
+++ crypto/data.py
@@ -0,0 +1,5 @@
+# ['dace.pundure@tietoevry.com', 'eriks.akasevs@tietoevry.com']
+
+TOKEN = '1867170994:AAGYLSm-x5EUKHwrkC5aaUIj0PbIsZiffYg'
+
+keys = {'Bitcoin': 'BTC', 'Euphonium': 'ETH', 'Dollar': 'USD'}
